3d76d5748f1eb14cf1528f436acbf5add2da640f
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-07-10 21:16:50 -0600

    display validation error messages on screen

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -15,7 +15,7 @@ export class BasicPageComponent implements OnInit {
   constructor( private fb: FormBuilder ) { }
   
   ngOnInit(): void {
-    this.myForm.reset( rtx5090 );
+    // this.myForm.reset( rtx5090 );
   }
 
   public myForm: FormGroup = this.fb.group({
@@ -25,7 +25,10 @@ export class BasicPageComponent implements OnInit {
   });
 
   onSave() {
-    if( this.myForm.invalid ) return;
+    if( this.myForm.invalid ) {
+      this.myForm.markAllAsTouched();
+      return;
+    };
     console.log(this.myForm.value);
 
     this.myForm.reset({ price: 10, inStorage: 0 });
