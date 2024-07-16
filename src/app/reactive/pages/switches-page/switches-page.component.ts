9e82e551ec687f11b77e6f12f91e569f800390a1
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-07-15 20:14:16 -0600

    task: connect form and validate fields

--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,9 +1,38 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './switches-page.component.html',
   styles: ``
 })
 export class SwitchesPageComponent {
+
+  constructor( private fb: FormBuilder ) { }
+
+  public myForm: FormGroup = this.fb.group({
+    gender: [ 'M', Validators.required ],
+    wantNotifications: [ true, Validators.required ],
+    termsAndCondition: [ false, Validators.requiredTrue ],
+  });
+
+  isValidField( field: string ): boolean | null {
+    return this.myForm.controls[field].errors 
+      && this.myForm.controls[field].touched;
+  }
+
+  onSave(): void {
+    if( this.myForm.invalid ) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myForm.value);
+    this.myForm.reset({
+      gender: 'M',
+      wantNotifications: false,
+      termsAndCondition: false
+    });
+
+  }
 
 }
