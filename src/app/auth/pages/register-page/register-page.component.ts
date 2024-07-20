52acbf2236b6330ba5c69355bb156934b5a40136
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-07-19 20:17:22 -0600

    asynchronous validators

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 // import { cantBeStrider, emailPattern, firstNameAndLastnamePattern } from '../../../shared/validators/validators';
 import { ValidatorsService } from '../../../shared/service/validators.service';
+import { EmailValidator } from '../../../shared/validators/email-validator.service';
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -9,12 +10,13 @@ import { ValidatorsService } from '../../../shared/service/validators.service';
 export class RegisterPageComponent {
   constructor(
     private fb: FormBuilder,
-    private validatorsService: ValidatorsService
+    private validatorsService: ValidatorsService,
+    private emailValidator: EmailValidator
   ) { }
 
   public myForm: FormGroup = this.fb.group({
     name: ['', [Validators.required, Validators.pattern( this.validatorsService.firstNameAndLastnamePattern ) ]],
-    email: ['', [ Validators.required, Validators.pattern( this.validatorsService.emailPattern ) ]],
+    email: ['', [ Validators.required, Validators.pattern( this.validatorsService.emailPattern ) ], [ this.emailValidator ]],
     username: ['', [ Validators.required, this.validatorsService.cantBeStrider ]],
     password: ['', [Validators.required, Validators.minLength(6)]],
     password2: ['', [Validators.required]],
