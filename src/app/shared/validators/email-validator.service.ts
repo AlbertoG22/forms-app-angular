23a8619a3354c0a7f731180c3f62c768a0c4c1b9
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-07-22 19:39:50 -0600

    asynchronous email validator

--- src/app/shared/validators/email-validator.service.ts
+++ src/app/shared/validators/email-validator.service.ts
@@ -5,15 +5,35 @@ import { Observable, delay, of } from 'rxjs';
 @Injectable({providedIn: 'root'})
 export class EmailValidator implements AsyncValidator {
 
+  // validate(control: AbstractControl): Observable<ValidationErrors | null> {
+  //   const email = control.value;
+  //   console.log({ email });
+
+  //   return of({
+  //     emailTaken: true
+  //   }).pipe(
+  //     delay( 2000 ) // para ver el status pending del formulario
+  //   );
+  // }
+
   validate(control: AbstractControl): Observable<ValidationErrors | null> {
     const email = control.value;
-    console.log({ email });
+    
+    const httpCallObservable = new Observable<ValidationErrors | null>( (subscriber) => {
+      console.log({ email });
 
-    return of({
-      emailTaken: true
+      if( email === 'alberto@google.com' ) {
+        subscriber.next({ emailTaken: true });
+        subscriber.complete();
+        // este return es opcional, ya que igual deja de emitir valores con el complete()
+        // return;
+      }
+      subscriber.next(null);
+      subscriber.complete();
     }).pipe(
-      delay( 2000 ) // para ver el status pending del formulario
+      delay(5000)
     );
+
+    return httpCallObservable;
   }
-  
 }
